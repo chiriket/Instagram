82efc87edd8c5d1e0ead8cb6aae2e627acc3e772
Shirley Keter <shirleyketer@gmail.com>
2019-05-19 21:33:27 +0300

    added imagefield

--- gram/views.py
+++ gram/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Post
 
 # Create your views here.
 def index(request):
