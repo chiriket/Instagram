7598358bfa11082d6feaa281232dccb050020b7a
Shirley Keter <shirleyketer@gmail.com>
2019-05-20 11:36:35 +0300

    added welcome email

--- instagram/settings.py
+++ instagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,6 +27,13 @@ SECRET_KEY = '*&t$432yjzfyk)5#ag=g)+$_4$-ober$c&-_0_*esb(r0si^ng'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 # Application definition
