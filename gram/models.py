3c4be964004965c1dbd9826b4b8cf48e449b7bd5
Shirley Keter <shirleyketer@gmail.com>
2019-05-20 08:17:54 +0300

    added comment

--- gram/models.py
+++ gram/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 
-# Create your models here.
-class Profile(models.Model):
-    # profile_photo = models.ImageField(upload_to = 'profilepics/', default='Image')
-    prof_pic = ImageField(blank=True, manual_crop='800x800')
-    bio = HTMLField()
-    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+# # Create your models here.
+# class Profile(models.Model):
+#     # profile_photo = models.ImageField(upload_to = 'profilepics/', default='Image')
+    
+#     bio = HTMLField()
+#     user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
 
-    def save_profile(self):
-        self.save()
+#     def save_profile(self):
+#         self.save()
     
-    @classmethod
-    def search_profile(cls, name):
-        profile = Profile.objects.filter(user__username__icontains = name)
-        return profile
+#     @classmethod
+#     def search_profile(cls, name):
+#         profile = Profile.objects.filter(user__username__icontains = name)
+#         return profile
     
-    @classmethod
-    def get_by_id(cls, id):
-        profile = Profile.objects.get(user = id)
-        return profile
+#     @classmethod
+#     def get_by_id(cls, id):
+#         profile = Profile.objects.get(user = id)
+#         return profile
 
 
 class Post(models.Model):
