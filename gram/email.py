b2e8a4df43d3a2b0fc61d9d1ac5419c98e7a825b
Shirley Keter <shirleyketer@gmail.com>
2019-05-23 13:00:38 +0300

    push to heroku

--- gram/email.py
+++ gram/email.py
@@ -1,5 +1,22 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode
+
+
+def send_activation_email(user, current_site, receiver):
+    subject = 'Activate your account'
+
+    message = render_to_string('registration/activate.html', {
+        'user':user,
+        'domain':current_site.domain,
+        'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+        # 'token':account_activation_token.make_token(user),
+    })
+
+    email = EmailMessage(subject, message, to=[receiver])
+    email.send()
 
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
